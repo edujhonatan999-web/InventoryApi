3c58bff4809b1329884ea0739dc7ac341b68dfb2
Daniel Torres <sd3danii@gmail.com>
2026-05-21 17:49:50 -0500

    CORS ENABLED

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://inventoryweb-production-30fd.up.railway.app',
+    ],
     credentials: true,
   });
   const port = process.env.PORT ? parseInt(process.env.PORT) : 4000;
   await app.listen(port);
   console.log(`Server is running on port ${port}`);
   console.log('JWT:', process.env.JWT_SECRET);
-  app.enableCors();
+  
 }
 bootstrap();
